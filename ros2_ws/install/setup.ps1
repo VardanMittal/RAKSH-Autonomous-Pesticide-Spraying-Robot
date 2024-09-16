fb7af075265e9a7d79c5c249e3e845033f9d14d4
VardanMittal <vardanmittal000@gmail.com>
2024-09-16 13:40:59 +0530

    raksh description files initialized and some urdf and xacro files are created for robot description

--- ros2_ws/install/setup.ps1
+++ ros2_ws/install/setup.ps1
@@ -25,6 +25,7 @@ function _colcon_prefix_chain_powershell_source_script {
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/vardan/Introduction-to-Robotics/Module2/ros2_ws/install\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/vardan/Introduction-to-Robotics/Module1/ros2_wk/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/vardan/OWL_Folder/Cloth_Folding/ros2_ws/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
--- ros2_ws/install/setup.zsh
+++ ros2_ws/install/setup.zsh
@@ -27,6 +27,9 @@ _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/home/vardan/Introduction-to-Robotics/Module1/ros2_wk/install"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
+# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
+COLCON_CURRENT_PREFIX="/home/vardan/OWL_Folder/Cloth_Folding/ros2_ws/install"
+_colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
